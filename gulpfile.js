1a0ab92371c9fc618e2c4f4e9a4c616f3a2af10b
Gashington <gashington@gmail.com>
2014-08-29 15:52:04 +0300

    add task "build" and subtasks buld-js, buld-css, img-min

--- gulpfile.js
+++ gulpfile.js
@@ -3,39 +3,17 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    connect = require ('gulp-connect')
+    connect = require ('gulp-connect'),
+    minifyCSS = require('gulp-minify-css');
 
 
-// Задача по умолчанию.
+// Задачи выполняемые при старте.
 gulp.task('default', function () {
-    gulp.start('server','stylus', 'watch'); // запуск сервера, компиляция stylus
-});
-// Задача по зборке проекта в папку app.
-
-gulp.task('watch', function () {
-    gulp.watch(['./source/*.html'], ['html']);
+    gulp.start('dev-server','stylus', 'watch'); // запуск сервера, компиляция stylus, запуск наблюдателей.
 });
 
-gulp.task('html', function () {
-    console.log(true)
-    gulp.src('dest/*.html')
-        .pipe(connect.reload());
-});
-
-gulp.task('stylus', function () {
-    gulp.src(['source/stylus/*.styl', '!source/stylus/mixin/*.styl'])
-        .pipe(stylus())
-        .pipe(gulp.dest('source/css'));
-});
-
-
-gulp.task('img-min', function () {
-    gulp.src('source/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('app/img'))
-});
-
-gulp.task('server', function(){
+// запуск сервера
+gulp.task('dev-server', function(){
     connect.server({
         root: ['./source'],
         port: 3000,
@@ -43,5 +21,72 @@ gulp.task('server', function(){
         livereload: true
     })
 });
+
+// Включаем наблюдателей в рабочей директории
+gulp.task('watch', function () {
+    gulp.watch(['./source/*.html'], ['html']); // наблюдение за файлами  *.html
+    gulp.watch(['./source/stylus/**/*.styl'], ['stylus']); // наблюдение за файлами  *.styl
+    gulp.watch(['./source/css/**/*.css'], ['css']); // наблюдение за файлами  *.css
+    gulp.watch(['./source/css/**/*.js'], ['js']); // наблюдение за файлами  *.js
+    gulp.watch(['./source/img/**/*.{png,jpg,gif}'], ['img']); // наблюдение за изображениями
+});
+
+    // перезагрузка страницы при изменении и добавлении *.html
+    gulp.task('html', function () {
+        console.log(true)
+        gulp.src('dest/*.html')
+            .pipe(connect.reload());
+    });
+
+    // перезагрузка страницы при изменении и добавлении изображений
+    gulp.task('img', function () {
+        //console.log(true)
+        gulp.src('./source/img/**/*.{png,jpg,gif}')
+            .pipe(connect.reload());
+    });
+    // перезагрузка страницы при изменении и добавлении изображений
+    gulp.task('css', function () {
+        //console.log(true)
+        gulp.src('./source/css/**/*.css')
+            .pipe(connect.reload());
+    });
+
+    // перезагрузка страницы при изменении и добавлении .*js
+    gulp.task('js', function () {
+        //console.log(true)
+        gulp.src('./source/js/**/*.js')
+            .pipe(connect.reload());
+    });
+
+    // компиляция stylus
+    gulp.task('stylus', function () {
+        gulp.src(['source/stylus/*.styl', '!source/stylus/mixin/*.styl'])
+            .pipe(stylus())
+            .pipe(gulp.dest('source/css'))
+    });
+
+// Задача по зборке проекта в папку ./app.
+gulp.task('build', function () {
+    gulp.start('build-css','build-js', 'img-min');
+});
+
+    gulp.task('build-js', function() {
+        gulp.src(['./source/js/vendor/*jquery-1.11.1.js', './source/js/plugins.js', './source/js/main.js'])
+            .pipe(concat('build.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'));
+    });
+    gulp.task('build-css', function() {
+        gulp.src(['./source/css/vendor/normalize.css', './source/css/main.css', './source/css/widgets.css'])
+            .pipe(concat('build.min.css'))
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('app/css'));
+    });
+    gulp.task('img-min', function () {
+        gulp.src('source/img/**/*.{png,jpg,gif}')
+            .pipe(imagemin())
+            .pipe(gulp.dest('app/img'))
+    });
+
 
 
