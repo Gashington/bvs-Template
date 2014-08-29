84d797b788395190521bd42126178892071c7dec
Gashington <gashington@gmail.com>
2014-08-29 16:45:38 +0300

    add subtasks copy-css, copy-js, copy-html

--- gulpfile.js
+++ gulpfile.js
@@ -65,27 +65,56 @@ gulp.task('watch', function () {
             .pipe(gulp.dest('source/css'))
     });
 
-// Задача по зборке проекта в папку ./app.
+// Задача по сборке проекта в папку ./app.
 gulp.task('build', function () {
-    gulp.start('build-css','build-js', 'img-min');
+    gulp.start([
+        'build-css',
+        'build-js',
+        'img-min',
+        'copy-html',
+        'copy-css',
+        'copy-js'
+    ]);
 });
-
+    // сборка js
     gulp.task('build-js', function() {
         gulp.src(['./source/js/vendor/*jquery-1.11.1.js', './source/js/plugins.js', './source/js/main.js'])
             .pipe(concat('build.min.js'))
             .pipe(uglify())
             .pipe(gulp.dest('app/js'));
     });
+
+    // сборка css
     gulp.task('build-css', function() {
         gulp.src(['./source/css/vendor/normalize.css', './source/css/main.css', './source/css/widgets.css'])
             .pipe(concat('build.min.css'))
             .pipe(minifyCSS())
             .pipe(gulp.dest('app/css'));
     });
+
+    // оптимизация изображений
     gulp.task('img-min', function () {
-        gulp.src('source/img/**/*.{png,jpg,gif}')
+        gulp.src('./source/img/**/*.{png,jpg,gif}')
             .pipe(imagemin())
-            .pipe(gulp.dest('app/img'))
+            .pipe(gulp.dest('./app/img'))
+    });
+
+    // копирование *.html в папку проекта
+    gulp.task('copy-html', function () {
+        gulp.src(['./source/*.html'], { base: './source' })
+            .pipe(gulp.dest('./app'))
+    });
+
+    // копирование *.css в папку проекта
+    gulp.task('copy-css', function () {
+        gulp.src(['./source/css/**/*.css'], { base: './source/css' })
+            .pipe(gulp.dest('./app/css'))
+    });
+
+    // копирование *.js в папку проекта
+    gulp.task('copy-js', function () {
+        gulp.src(['./source/js/**/*.js'], { base: './source/js' })
+            .pipe(gulp.dest('./app/js'))
     });
 
 
